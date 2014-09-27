3c11283bcdce7494e5afef4bfee234755ecb6719
Christian Grant <grantc@augsburg.edu>
2014-09-27 04:26:18 -0500

    API Attempts

--- ProductID/src/productid/ProductID.java
+++ ProductID/src/productid/ProductID.java
@@ -6,17 +6,13 @@
 package productid;
 
 
-import java.util.List;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.method.HttpGet;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
@@ -28,11 +24,11 @@ public class ProductID{
     
     public static void main (String[] args) throws ClientProtocolException, IOException {
         HttpClient client = new DefaultHttpClient();
-        HttpGet request = HttpGet("api.target.com");
+        HttpGet request = new HttpGet("api.target.com");
         HttpResponse response = client.execute(request);
         BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-        String line = '';
-        while ((line - rd.readLine() != null)){
+        String line;
+        while ((line = rd.readLine()) != null){
             System.out.println(line);
         }
     }
