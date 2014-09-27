7cb4610c283c5d16b2315c5af2dee9e7572a4982
Christian Grant <grantc@augsburg.edu>
2014-09-27 08:10:07 -0500

    Store Parser additional comments

--- src/targetParser/StoreParser.java
+++ src/targetParser/StoreParser.java
@@ -26,7 +26,8 @@ public class ProductParser{
 			int count = 0;
 
 			temp = data.substring(data.indexOf("AvailablityStatus")+8);
-						//Once first letter is determined, use it to just send back a boolean value to represent availability. 
+						//Once first letter is determined, use it to just send back a boolean value to represent availability. A is true, N is false.
+						//Even better A is true, not A is false.
                         status = temp.substring(0,temp.indexOf(",")-1);
 
 	}
